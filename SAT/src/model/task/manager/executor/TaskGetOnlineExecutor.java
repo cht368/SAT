3a98eff272d05e500b64d125db5e313718f17cb8
cht368 <christianthomas368@gmail.com>
2016-11-06 11:52:51 +0800

    commit task executor 3 biji
    get online, login, logout

--- SAT/src/model/task/manager/executor/TaskGetOnlineExecutor.java
+++ SAT/src/model/task/manager/executor/TaskGetOnlineExecutor.java
@@ -5,10 +5,25 @@
  */
 package model.task.manager.executor;
 
+import java.net.Socket;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import model.packet.Packet;
+
 /**
  *
  * @author Ega Prianto
  */
-public class TaskGetOnlineExecutor {
+public class TaskGetOnlineExecutor extends TaskExecutor{
+
+    public TaskGetOnlineExecutor(ConcurrentHashMap<String, Socket> connectedSockets, CopyOnWriteArrayList<Socket> connectedServerSockets, Packet packet) {
+        super(connectedSockets, connectedServerSockets, packet);
+    }
+
+    @Override
+    public void run() {
+        //TODO ambil data user yang sedang online dari database
+        // TODO kirim daftar user yang sedang online ke client
+    }
     
 }
--- SAT/src/model/task/manager/executor/TaskLoginExecutor.java
+++ SAT/src/model/task/manager/executor/TaskLoginExecutor.java
@@ -5,10 +5,37 @@
  */
 package model.task.manager.executor;
 
+import java.net.Socket;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import model.packet.Packet;
+import model.packet.PacketLoginClient;
+
 /**
  *
  * @author Ega Prianto
  */
-public class TaskLoginExecutor {
+public class TaskLoginExecutor extends TaskExecutor{
+
+    public TaskLoginExecutor(ConcurrentHashMap<String, Socket> connectedSockets, CopyOnWriteArrayList<Socket> connectedServerSockets, Packet packet) {
+        super(connectedSockets, connectedServerSockets, packet);
+    }
+
+    @Override
+    public void run() {
+        PacketLoginClient packetLogin = (PacketLoginClient) this.packet;
+        
+//        if (packetLogin.id exist in database) {
+//            if (packetLogin.password == 'select password from database where id = packetLogin.id') {
+//                //berhasil login
+//                // sql update table set status=online where id=packetLogin.id
+//            }else{
+//                //kirim pesan ke client, password salah  
+//            }
+//        }
+//        else{
+//               //kirim pesan ke client, id salah
+//        }
+    }
     
 }
